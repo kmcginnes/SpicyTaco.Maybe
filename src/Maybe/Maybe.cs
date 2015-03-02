6ebac054cccc1753f25952b73b6d97dafdeda663
Kris McGinnes <kmcginnes@me.com>
2015-03-01 20:36:57 -0600

    Added ToMaybe and equality

--- src/Maybe/Maybe.cs
+++ src/Maybe/Maybe.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Maybe
 {
@@ -16,9 +14,14 @@ namespace Maybe
         {
             return Maybe<T>.Create(value);
         }
+
+        public static Maybe<T> ToMaybe<T>(this T value) where T : class
+        {
+            return Maybe<T>.Create(value);
+        }
     }
 
-    public class Maybe<T> where T : class
+    public class Maybe<T> : IEquatable<Maybe<T>> where T : class
     {
         public static Maybe<T> Empty = new Maybe<T>();
 
@@ -32,6 +35,54 @@ namespace Maybe
         Maybe(T value)
         {
             _value = value;
+        }
+
+        public bool Equals(Maybe<T> other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return EqualityComparer<T>.Default.Equals(_value, other._value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Maybe<T>)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return EqualityComparer<T>.Default.GetHashCode(_value);
+        }
+
+        public static bool operator ==(Maybe<T> left, Maybe<T> right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Maybe<T> left, Maybe<T> right)
+        {
+            return !Equals(left, right);
+        }
+
+        public override string ToString()
+        {
+            var typeName = typeof (T).Name;
+            return HasValue 
+                ? Value.ToString()
+                : String.Format("Maybe<{0}> has no value", typeName);
+        }
+
+        public static implicit operator Maybe<T>(T value)
+        {
+            return Create(value);
+        }
+
+        public static implicit operator T(Maybe<T> maybe)
+        {
+            return maybe.Value;
         }
 
         readonly T _value;
